13e8b1818e381529d1321a487e7d8c13d5400f13
sangeetakuntoji <139916774+sangeetakuntoji@users.noreply.github.com>
2024-03-23 14:17:57 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_mssql_server" "example" {
   }
 }
 
-resource "azurerm_mssql_database" "example" {
+resource "azurerm_sql_database" "example" {
   name                = "myexamplesqldatabase"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
