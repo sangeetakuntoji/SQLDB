0bb22ef40959e4092301ecf521698545e7487b42
sangeeta <sangeeta.kuntoji@gmail.com>
2024-03-24 09:21:42 +0100

    hja

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 #terraform script
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource-group-name
-  location = var.location
+  name     = "testdemo"
+  location = "west europe"
 }
 
 resource "azurerm_app_service_plan" "plan" {
   name                = "example-appserviceplan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "west europe"
+  resource_group_name = "testdemo"
 
   sku {
     tier = "Standard"
@@ -17,15 +17,15 @@ resource "azurerm_app_service_plan" "plan" {
 
 resource "azurerm_app_service" "app-service" {
   name                = var.app-service-name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "west europe"
+  resource_group_name = "testdemo"
   app_service_plan_id = azurerm_app_service_plan.plan.id
 }
 
 resource "azurerm_sql_server" "sqldb" {
   name                         = "terraform-sqlserver"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = "testdemo"
+  location                     = "west europe"
   version                      = "12.0"
   administrator_login          = "houssem"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
@@ -33,8 +33,8 @@ resource "azurerm_sql_server" "sqldb" {
 
 resource "azurerm_sql_database" "db" {
   name                = "terraform-sqldatabase"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = "testdemo"
+  location            = "west europe"
   server_name         = azurerm_sql_server.sqldb.name
 
   tags = {
