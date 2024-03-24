2556e38031a52c8b8940cb85592adf7a09b8496b
sangeeta <sangeeta.kuntoji@gmail.com>
2024-03-24 09:12:35 +0100

    done

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,23 +20,8 @@ resource "azurerm_app_service" "app-service" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.sqldb.fully_qualified_domain_name} Database=${azurerm_sql_database.db.name};User ID=${azurerm_sql_server.sqldb.administrator_login};Password=${azurerm_sql_server.sqldb.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
-  }
 }
-
+ 
 resource "azurerm_sql_server" "sqldb" {
   name                         = "terraform-sqlserver"
   resource_group_name          = azurerm_resource_group.rg.name
